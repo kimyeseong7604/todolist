2fc48c0ebce3129dfc4af39384e6e2c2aa90ad3e
kimyesoung <kycys1834@naver.com>
2025-11-13 19:30:06 +0900

    edit 25-11-13

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-{
-  /*
-    작성자 : 홍길동
-    작성일 : 2030.11.23
-    내용 : 기능에 대한 내용
-  */
-}
+import TodoList from './Todolist';
+import MapTest from './MapTest';
+import Clock from './Timer';
 
 function App() {
   let name = "대상혁";
 
   return (
     <div className="container">
-      <h1 className="test">hello,
-        {
-          name === '리액트' ? (<h1>YES</h1>) : null
-        }!!</h1>
-      <p>반갑습니다.</p>
-      <br/>
-      <input/>
+      <TodoList />
+      <MapTest />
+      <Clock />
+
     </div>
   );
 }
